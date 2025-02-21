e26777a30ec04268c13c84993bfd73f33182c0d3
artemgrishaev <artemail05@gmail.com>
2025-02-21 14:16:31 +0300

    Initial

---
+++ src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/")
+    public String index(Model model){
+
+//        model.addAttribute("rows", studentService.getAll());
+
+        model.addAttribute("rows", studentService.getAll());
+        model.addAttribute("new_student", new Student());
+
+
+        return "index";
+    }
+
+    @PostMapping("/add")
+    String add(@ModelAttribute Student new_student){
+        studentService.save(new_student);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    String delete(@PathVariable("id") int id){
+        studentService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getAll(){
+        return studentRepository.findAll();
+    }
+
+    void save(Student student){studentRepository.save(student);}
+
+    void delete(int id){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid user Id: " + id));
+        studentRepository.delete(student);
+    }
+
+}
